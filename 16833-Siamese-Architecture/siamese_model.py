aa5c394a2c8fd8a56557933b6db8f37faf472847
ssajeev <sandrasajeev@gmail.com>
2019-10-20 12:54:44 -0400

    updated with batchnorm and relu between conv

--- 16833-Siamese-Architecture/siamese_model.py
+++ 16833-Siamese-Architecture/siamese_model.py
@@ -9,48 +9,71 @@ class DepthModel(nn.Module):
 
     #Encoder
     self.conv1_1 = nn.Conv2d(3, 64, 3, 1)
-    self.relu1 = nn.ReLU
-    self.batchnorm_1 = nn.BatchNorm2d(64, 1e-3)
+    self.relu1_1 = nn.ReLU()
+    self.batchnorm_1_1 = nn.BatchNorm2d(64, 1e-3)
 
     self.conv1_2 = nn.Conv2d(64, 64, 3, 1)
-    self.relu1 = nn.ReLU
-    self.batchnorm_1 = nn.BatchNorm2d(64, 1e-3)
+    self.relu1_2 = nn.ReLU()
+    self.batchnorm_1_2 = nn.BatchNorm2d(64, 1e-3)
 
     self.pool1 = nn.MaxPool2d(2, 2, return_indices=True)
+
     self.conv2_1 = nn.Conv2d(64, 128, 3, 1)
-    self.relu1 = nn.ReLU
-    self.batchnorm_1 = nn.BatchNorm2d(64, 1e-3)
+    self.relu2_1 = nn.ReLU()
+    self.batchnorm_2_1 = nn.BatchNorm2d(128, 1e-3)
 
     self.conv2_2 = nn.Conv2d(128, 128, 3, 1)
-    self.relu1 = nn.ReLU
-    self.batchnorm_1 = nn.BatchNorm2d(64, 1e-3)
+    self.relu2_2 = nn.ReLU()
+    self.batchnorm_2_2 = nn.BatchNorm2d(128, 1e-3)
 
     self.pool2 = nn.MaxPool2d(2, 2, return_indices=True)
+
     self.conv3_1 = nn.Conv2d(128, 256, 3, 1)
-    self.relu1 = nn.ReLU
-    self.batchnorm_1 = nn.BatchNorm2d(64, 1e-3)
+    self.relu3_1 = nn.ReLU()
+    self.batchnorm_3_1 = nn.BatchNorm2d(256, 1e-3)
 
     self.conv3_2 = nn.Conv2d(256, 256, 3, 1)
-    self.relu1 = nn.ReLU
-    self.batchnorm_1 = nn.BatchNorm2d(64, 1e-3)
+    self.relu3_2 = nn.ReLU()
+    self.batchnorm_3_2 = nn.BatchNorm2d(256, 1e-3)
 
     self.conv3_3 = nn.Conv2d(256, 256, 3, 1)
-    self.relu1 = nn.ReLU
-    self.batchnorm_1 = nn.BatchNorm2d(64, 1e-3)
+    self.relu3_3 = nn.ReLU()
+    self.batchnorm_3_3 = nn.BatchNorm2d(256, 1e-3)
 
     self.pool3 = nn.MaxPool2d(2, 2, return_indices=True)
+
     self.conv4_1 = nn.Conv2d(256, 512, 3, 1)
-    self.relu1 = nn.ReLU
-    self.batchnorm_1 = nn.BatchNorm2d(64, 1e-3)
+    self.relu4_1 = nn.ReLU()
+    self.batchnorm_4_1 = nn.BatchNorm2d(512, 1e-3)
 
     self.conv4_2 = nn.Conv2d(512, 512, 3, 1)
+    self.relu4_2 = nn.ReLU()
+    self.batchnorm_4_2 = nn.BatchNorm2d(512, 1e-3)
+
     self.conv4_3 = nn.Conv2d(512, 512, 3, 1)
+    self.relu4_3 = nn.ReLU()
+    self.batchnorm_4_3 = nn.BatchNorm2d(512, 1e-3)
+
     self.pool4 = nn.MaxPool2d(2, 2, return_indices=True)
+
     self.conv5_1 = nn.Conv2d(512, 512, 3, 1)
+    self.relu5_1 = nn.ReLU()
+    self.batchnorm_5_1 = nn.BatchNorm2d(512, 1e-3)
+
     self.conv5_2 = nn.Conv2d(512, 512, 3, 1)
+    self.relu5_2 = nn.ReLU()
+    self.batchnorm_5_2 = nn.BatchNorm2d(512, 1e-3)
+
     self.conv5_3 = nn.Conv2d(512, 512, 3, 1)
+    self.relu5_3 = nn.ReLU()
+    self.batchnorm_5_3 = nn.BatchNorm2d(512, 1e-3)
+
     self.pool5 = nn.MaxPool2d(2, 2, return_indices=True)
+
     self.conv6 = nn.Conv2d(512, 512, 3, 1)
+    self.relu6 = nn.ReLU()
+    self.batchnorm6 = nn.BatchNorm2d(512, 1e-3)
+
 
     #Decoder
     self.unpool5 = nn.MaxUnpool2d(2, 2)
