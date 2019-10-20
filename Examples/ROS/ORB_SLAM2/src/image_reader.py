a259066e0cbc88efbb37c92fcbf29a743618d470
Advaith Sethuraman <advaiths@gmail.com>
2019-10-20 11:51:13 -0400

    hi

---
+++ Examples/ROS/ORB_SLAM2/src/image_reader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+ 
+  import roslib
+  roslib.load_manifest('my_package')
+  import sys
+  import rospy
+  import cv2
+  from std_msgs.msg import String
+  from sensor_msgs.msg import Image
+  from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_grabber:
+	def __init__(self)
+
